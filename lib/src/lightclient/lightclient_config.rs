f272f3b5df9204b5576cd3ffcd729b64131e40df
zancas <zancas@zingolabs.org>
2022-05-21 00:52:11 -0600

    if this doc comment is correct this is test-only code

--- lib/src/lightclient/lightclient_config.rs
+++ lib/src/lightclient/lightclient_config.rs
@@ -45,6 +45,7 @@ pub struct LightClientConfig {
 
 impl LightClientConfig {
     // Create an unconnected (to any server) config to test for local wallet etc...
+    #[cfg(test)]
     pub fn create_unconnected(chain_name: String, dir: Option<String>) -> LightClientConfig {
         LightClientConfig {
             server: http::Uri::default(),
